8caa3fd0402a1085447c094e7bd50f3f9f24d885
whaleOpop <al-agabani-kit@yandex.ru>
2022-04-10 20:14:10 +0300

    Initial commit

---
+++ src/main/java/doublewhaleapi/dwapi/DWAPI.java
@@ -0,0 +1,17 @@
+package doublewhaleapi.dwapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DWAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
